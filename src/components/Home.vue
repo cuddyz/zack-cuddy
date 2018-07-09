87722a93fcf00810e5640eeaaa0fb79587e6aa59
Cuddy <zcuddy23@gmail.com>
2018-07-09 10:57:11 -0500

    Remove template html

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <section>
+      <h1>Zack Cuddy</h1>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      isLoading: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  @import '../styles/colors.scss';
+  h1 {
+    color: color(blue);
+  }
+</style>

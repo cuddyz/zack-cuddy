f15ca4c4acf1b0163700c8a4ca83dbad79f527a6
Cuddy <zcuddy23@gmail.com>
2018-08-01 13:36:50 -0500

    Contact component

---
+++ src/components/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>Contact Me</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../styles/colors.scss';
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,7 @@
     <profile class="m-3"></profile>
     <skills class="pr-3 pl-3"></skills>
     <projects class="m-3"></projects>
+    <contact class="m-3"></contact>
   </main>
 </template>
 
@@ -10,18 +11,15 @@
 import Profile from '@/components/Profile.vue'
 import Skills from '@/components/Skills.vue'
 import Projects from '@/components/Projects.vue'
+import Contact from '@/components/Contact.vue'
 
 export default {
   name: 'Home',
-  data () {
-    return {
-      isLoading: false
-    }
-  },
   components: {
     Profile,
     Skills,
-    Projects
+    Projects,
+    Contact
   }
 }
 </script>

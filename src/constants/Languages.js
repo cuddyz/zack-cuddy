441494d226ec1edaaf78485bd48f292e724e0830
Zack Cuddy <zcuddy23@gmail.com>
2018-07-31 21:48:51 -0500

    Testimonials

--- src/constants/Languages.js
+++ src/constants/Languages.js
@@ -4,7 +4,7 @@ const VUE = {
   website: 'https://vuejs.org/',
   image: 'vue.png',
   description: '<p>Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable. The core library is focused on the view layer only, and is easy to pick up and integrate with other libraries or existing projects. On the other hand, Vue is also perfectly capable of powering sophisticated Single-Page Applications when used in combination with modern tooling and supporting libraries.</p>',
-  testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus quis dolor non tempor. Nulla eros ex, egestas eget rhoncus non, tempor non diam. Fusce nibh ante, varius id massa eu, maximus porttitor massa. Phasellus fermentum ornare felis sed dapibus. Curabitur et luctus ante, nec hendrerit urna'
+  testimonial: 'VueJS is currently my favorite front end framework to work in.  I made this whole site in it!  My roots of web development live in AngularJS 1.6, and Vue in many ways feels like everything Angular should or could have been.  The well drawn out component structure makes creating Vue apps a breeze.  On top of that, the “lighter” reactivity makes them run fast and efficient even as your application grows.'
 }
 
 const NODE = {
@@ -13,7 +13,7 @@ const NODE = {
   website: 'https://nodejs.org',
   image: 'nodejs.png',
   description: '<p style="padding-bottom: 1rem;">Node.js® is a JavaScript runtime built on Chrome’s V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient.</p> <p>Node.js’ package ecosystem, npm, is the largest ecosystem of open source libraries in the world.</p>',
-  testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus quis dolor non tempor. Nulla eros ex, egestas eget rhoncus non, tempor non diam. Fusce nibh ante, varius id massa eu, maximus porttitor massa. Phasellus fermentum ornare felis sed dapibus. Curabitur et luctus ante, nec hendrerit urna'
+  testimonial: 'NodeJS has become the sole server-side language for any new projects being spun up at my current employer.  Node brings a whole new view into writing your APIs.  The biggest draw to it for me is that it is JavaScript! Having your front and back end in the same core language saves a lot of time and energy when switching context between different parts of your app.  On top of that it is fast and gives you access to the ever so helpful NPM libraries.'
 }
 
 const ANGULAR = {
@@ -22,7 +22,7 @@ const ANGULAR = {
   website: 'https://angularjs.org/',
   image: 'angular.png',
   description: '<p style="padding-bottom: 1rem;">AngularJS is a structural framework for dynamic web apps. It lets you use HTML as your template language and lets you extend HTML\'s syntax to express your application\'s components clearly and succinctly. AngularJS\'s data binding and dependency injection eliminate much of the code you would otherwise have to write. And it all happens within the browser, making it an ideal partner with any server technology.</p> <p>AngularJS is what HTML would have been, had it been designed for applications. HTML is a great declarative language for static documents. It does not contain much in the way of creating applications, and as a result building web applications is an exercise in what do I have to do to trick the browser into doing what I want?</p>',
-  testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus quis dolor non tempor. Nulla eros ex, egestas eget rhoncus non, tempor non diam. Fusce nibh ante, varius id massa eu, maximus porttitor massa. Phasellus fermentum ornare felis sed dapibus. Curabitur et luctus ante, nec hendrerit urna'
+  testimonial: 'It was a sad day when I read online that AngularJS was going away.  Angular was where I learned the basics of making a functional web app.  Thus sliding into VueJS came pretty naturally with Angular 1.x being its inspiration.  I enjoyed how “less” opinionated Angular 1.x was compared to the newer Angular 2/4/6. I also am very fond of the relatively painless process of upgrading Angular 1.x apps into VueJS apps, which I have done on more than a few different accounts.'
 }
 
 const HTML = {
@@ -31,7 +31,7 @@ const HTML = {
   website: 'https://www.w3.org/html/',
   image: 'html.png',
   description: '<p style="padding-bottom: 1rem;">HTML is the language for describing the structure of Web pages. HTML gives authors the means to:</p> <ul style="padding-bottom: 1rem;"> <li>Publish online documents with headings, text, tables, lists, photos, etc.</li><li>Retrieve online information via hypertext links, at the click of a button.</li><li>Design forms for conducting transactions with remote services, for use in searching for information, making reservations, ordering products, etc.</li> </ul> <p> With HTML, authors describe the structure of pages using markup. The elements of the language label pieces of content such as “paragraph,” “list,” “table,” and so on.</p>',
-  testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus quis dolor non tempor. Nulla eros ex, egestas eget rhoncus non, tempor non diam. Fusce nibh ante, varius id massa eu, maximus porttitor massa. Phasellus fermentum ornare felis sed dapibus. Curabitur et luctus ante, nec hendrerit urna'
+  testimonial: 'After a few years of being locked in front of a computer screen, HTML has almost become a second language for me.  The pure readability of it makes it really easy for even the non-technical users to understand.  It is a great tool for quickly shelling out information onto a web page.'
 }
 
 const CSS = {
@@ -40,7 +40,7 @@ const CSS = {
   website: 'https://www.w3.org/Style/CSS/',
   image: 'css.png',
   description: '<p>CSS is the language for describing the presentation of Web pages, including colors, layout, and fonts. It allows one to adapt the presentation to different types of devices, such as large screens, small screens, or printers. CSS is independent of HTML and can be used with any XML-based markup language. The separation of HTML from CSS makes it easier to maintain sites, share style sheets across pages, and tailor pages to different environments. This is referred to as the separation of structure (or: content) from presentation.</p>',
-  testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus quis dolor non tempor. Nulla eros ex, egestas eget rhoncus non, tempor non diam. Fusce nibh ante, varius id massa eu, maximus porttitor massa. Phasellus fermentum ornare felis sed dapibus. Curabitur et luctus ante, nec hendrerit urna'
+  testimonial: 'CSS to me often feels like the bane of my existence.  Constantly, I find myself getting trapped in a rabbit hole of “getting this one element just right”, to reorganizing the whole page an hour later.  Learning post-css frameworks like scss and less has really upped my organizational skills with how I layout my styles.  Of course the best part of getting a skill set in CSS is having the ability to “wow” your friends with a little inspector magic on their favorite websites!'
 }
 
 const JS = {
@@ -49,7 +49,7 @@ const JS = {
   website: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
   image: 'js.png',
   description: '<p>JavaScript (JS) is a lightweight interpreted or JIT-compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.</p>',
-  testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur finibus quis dolor non tempor. Nulla eros ex, egestas eget rhoncus non, tempor non diam. Fusce nibh ante, varius id massa eu, maximus porttitor massa. Phasellus fermentum ornare felis sed dapibus. Curabitur et luctus ante, nec hendrerit urna'
+  testimonial: 'Do a quick google search for “programming languages”.  I would be shocked if JavaScript didn’t come up in the first 5 results.  There is no question about the popularity of JavaScript right now.  It is all the hotness.  I listed 3 frameworks just on this one page!  My core skill set lives in JavaScript and the pure flexibility around it is why it is so popular.  Of course, all the flexibility has its shortcomings, but all in all I find that I really enjoy writing JavaScript on a day to day basis.'
 }
 
 export const Languages = {

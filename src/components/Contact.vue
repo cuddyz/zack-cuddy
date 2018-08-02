c07cc94c68b79e1cc3533f26c2324d5dc14286fe
Zack Cuddy <zcuddy23@gmail.com>
2018-08-01 19:13:40 -0500

    Formspree

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,13 +1,15 @@
 <template>
   <section class="grid">
     <h3 class="pt-1 pb-1">Need help with a project?</h3>
-    <input :class="{'error': errors.fields && !contact.name }" v-model="contact.name" type="text" placeholder="Full Name" />
-    <input :class="{'error': (errors.fields && !contact.email) || errors.email }" v-model="contact.email" type="email" placeholder="Email" />
-    <textarea :class="{'error': errors.fields && !contact.details }" v-model="contact.details" placeholder="Details" />
+    <form id="contactForm" action="https://formspree.io/zcuddy23@gmail.com" method="POST" class="grid">
+      <input name="name" :class="{'error': errors.fields && !contact.name }" v-model="contact.name" type="text" placeholder="Full Name" />
+      <input name="email" :class="{'error': (errors.fields && !contact.email) || errors.email }" v-model="contact.email" type="email" placeholder="Email" />
+      <textarea name="details" :class="{'error': errors.fields && !contact.details }" v-model="contact.details" placeholder="Details" />
+    </form>
     <button @click="submit" class="dark">Submit</button>
     <h4 class="error" v-if="errors.fields">Make sure all the fields are filled out.</h4>
     <h4 class="error" v-if="errors.email">That email doesn't look valid to me.</h4>
-    <h4 v-if="submitted">Thank you! I will reach out to you soon!</h4>
+    <h4 class="error" v-if="errors.post">Uh oh! Something went wrong with the POST. I console.log() it if you want to take a look!</h4>
   </section>
 </template>
 
@@ -24,8 +26,7 @@ export default {
       errors: {
         fields: false,
         email: false
-      },
-      submitted: false
+      }
     }
   },
   methods: {
@@ -33,8 +34,7 @@ export default {
       if (!this.validForm()) {
         return
       }
-
-      this.submitted = true
+      document.getElementById('contactForm').submit()
     },
     validForm: function() {
       if (!this.contact.name || !this.contact.email || !this.contact.details) {
@@ -69,9 +69,14 @@ export default {
       font-size: 1.3em;
     }
 
+    form {
+      text-align: center;
+      width: 50%;
+      grid-gap: 1rem;
+    }
+
     input,
     textarea {
-      width: 50%;
       font-size: 1.2rem;
 
       &.error {

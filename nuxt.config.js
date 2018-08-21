d30228b4ce0ffae302e3626e834d9fecc80e0251
Zack Cuddy <zcuddy23@gmail.com>
2018-08-20 21:43:36 -0500

    Shop plugin (/shop-plugin) (#10)
    * Shop plugin
    
    * Fix hover
    
    * Shopping cart in top right
    
    * Exact routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto|Material+Icons'},
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.1.0/css/all.css', integrity: 'sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt', crossorigin: 'anonymous'}
     ],
     script: [

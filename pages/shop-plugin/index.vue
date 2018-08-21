22c13eee21374efff3bca5aad816e6b06f0ac6b8
Cuddy <zcuddy23@gmail.com>
2018-08-20 21:37:27 -0500

    Shopping cart in top right

--- pages/shop-plugin/index.vue
+++ pages/shop-plugin/index.vue
@@ -1,32 +1,40 @@
 <template>
-  <div class="grid p-2">
-    <md-card :key="index" v-for="(item, index) in items">
-      <md-card-area>
-        <md-card-media>
-          <img :src="item.image">
-        </md-card-media>
+  <main>
+    <md-tabs md-active-tab="null" md-alignment="right">
+      <md-tab id="tab-cart" md-icon="shopping_cart"></md-tab>
+    </md-tabs>
+    <div class="cart-count">
+      <p>{{ cart.length }}</p>
+    </div>
+    <div class="grid p-2">
+      <md-card :key="index" v-for="(item, index) in items">
+        <md-card-area>
+          <md-card-media>
+            <img :src="item.image">
+          </md-card-media>
 
-        <md-card-header>
-          <h2 class="md-title">{{ item.title }}</h2>
-        </md-card-header>
+          <md-card-header>
+            <h2 class="md-title">{{ item.title }}</h2>
+          </md-card-header>
+
+          <md-card-content>
+            {{ item.description }}
+          </md-card-content>
+        </md-card-area>
 
         <md-card-content>
-          {{ item.description }}
+          <h3 class="md-subheading">Price</h3>
+          <div>
+            <p>$ {{ item.price }}</p>
+          </div>
         </md-card-content>
-      </md-card-area>
 
-      <md-card-content>
-        <h3 class="md-subheading">Price</h3>
-        <div>
-          <p>$ {{ item.price }}</p>
-        </div>
-      </md-card-content>
-
-      <md-card-actions class="pb-1">
-        <md-button class="md-primary">Add to cart</md-button>
-      </md-card-actions>
-    </md-card>
-  </div>
+        <md-card-actions class="pb-1">
+          <md-button @click="addToCart(item)" class="md-primary">Add to cart</md-button>
+        </md-card-actions>
+      </md-card>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -34,6 +42,7 @@ export default {
   name: 'ShopPlugin',
   data() {
     return {
+      cart: [],
       items: [
         {
           title: 'Wireless Headphones',
@@ -67,12 +76,18 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    addToCart(item) {
+      this.cart.push({...item})
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
   @import '../../assets/styles/breaks.scss';
+  @import '../../assets/styles/colors.scss';
 
   .grid {
     width: 100%;
@@ -95,5 +110,13 @@ export default {
       display: grid;
       grid-auto-rows: 5fr 1fr 2rem;
     }
+  }
+  .cart-count {
+    background-color: rgba(color(greyLight), .3);
+    padding: .15rem .45rem;
+    border-radius: 100%;
+    position: absolute;
+    top: .75rem;
+    right: .25rem;
   }
 </style>
